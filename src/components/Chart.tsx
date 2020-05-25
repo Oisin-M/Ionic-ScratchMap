7335f68f59421af6481f83a480184a3725838f85
Oisin-M <oisinmmm@gmail.com>
2020-05-25 18:22:54 +0100

    works, but very laggy

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -20,12 +20,6 @@ interface LooseObject {
   [key: string]: any
 }
 
-interface country_datum {
-  title: string;
-  id: string;
-  visited: string;
-}
-
 
 class Chart extends Component {
   componentDidMount() {
@@ -45,212 +39,71 @@ class Chart extends Component {
       chart.zoomControl = new am4maps.ZoomControl();
 
       // Center on the groups by default
-      chart.homeZoomLevel = 3.5;
-      chart.homeGeoPoint = { longitude: 10, latitude: 52 };
+      chart.homeZoomLevel = 1;
+      chart.homeGeoPoint = { longitude: 25, latitude: 25 };
 
       // This array will be populated with country IDs to exclude from the world series
       var excludedCountries: string[] = [];//["AQ"];
 
+      var init_data : LooseObject=chart.geodata
 
 
+      var visited_countries : any=[];
+      var unvisited_countries=[];
+
+      for (var i=0; i<init_data.features.length; i++) {
+        unvisited_countries.push(
+          init_data.features[i].id);
+      }
+
+      /* Northern Europe */
+      var unvisited = chart.series.push(new am4maps.MapPolygonSeries());
+      unvisited.name = "Unvisited";
+      unvisited.useGeodata = true;
+      unvisited.include = unvisited_countries;
+      unvisited.mapPolygons.template.tooltipText = "{name} ({id}): Unvisited";
+      unvisited.mapPolygons.template.fill = am4core.color("#96BDC6");
+      unvisited.fill = am4core.color("#96BDC6");
+
+      /* Central Europe */
+      var visited = chart.series.push(new am4maps.MapPolygonSeries());
+      visited.name = "Visited";
+      visited.useGeodata = true;
+      visited.include = visited_countries;
+      visited.mapPolygons.template.tooltipText = "{name} ({id}): Visited";
+      visited.mapPolygons.template.fill = am4core.color("#81968F");
+      visited.fill = am4core.color("#81968F");
 
 
+      unvisited.mapPolygons.template.events.on("hit", function(event) {
+        var clicked_place : LooseObject = event.target.dataItem.dataContext;
+        console.log(clicked_place);
+        unvisited.data=[];
+        var minus_incls=unvisited.include.slice()
+        minus_incls.splice(minus_incls.indexOf(clicked_place.id),1);
+        unvisited.include=minus_incls;
 
-    //   // The rest of the world.
-    //   var test = new am4maps.MapPolygonSeries();
-    //   var worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
-    //   var worldSeriesName = "world";
-    //   worldSeries.name = worldSeriesName;
-    //   worldSeries.useGeodata = true;
-    //   worldSeries.exclude = excludedCountries;
-    //   worldSeries.fillOpacity = 0.8;
-    //   worldSeries.hiddenInLegend = true;
-    //   worldSeries.mapPolygons.template.nonScalingStroke = true;
-      
-    //   console.log(worldSeries);
-    //   console.log(worldSeries.data);
-    //   console.log(worldSeries.data.length);
+        var plus_incls=visited.include.slice();
+        plus_incls.push(clicked_place.id);
+        visited.include=plus_incls;
+      })
 
-    //   console.log(String(worldSeries.data));
+      visited.mapPolygons.template.events.on("hit", function(event) {
+        var clicked_place : LooseObject = event.target.dataItem.dataContext;
+        console.log(clicked_place);
+        visited.data=[];
+        var minus_incls=visited.include.slice()
+        minus_incls.splice(minus_incls.indexOf(clicked_place.id),1);
+        visited.include=minus_incls;
 
-    // for (var entry in worldSeries.data) {
-    //   console.log(entry);
-    // }
-
-    //   for (var k=0; k<worldSeries.data.length; k++) {
-    //     console.log(worldSeries.data[k]);
-    //     console.log(worldSeries.data[k].name, worldSeries.data[k].id);
-    //   }
-
-    //   // console.log(worldSeries.dataItems.values);
-
-    //   console.log(chart);
-    //   var worldmapdata : LooseObject = chart.geodata
-    //   console.log(worldmapdata);
-    //   for (var k=0; k<worldmapdata.features.length; k++) {
-    //     console.log(worldmapdata.features[k]);
-    //      console.log(worldmapdata.features[k].properties.name, worldmapdata.features[k].properties.id);
-    //   }
-      // console.log("test", test);
-
-
-    var init_data : LooseObject=chart.geodata
-    console.log("geodata", init_data.features);
-
-
-    var visited_countries : Array<country_datum>=[];
-    var unvisited_countries: Array<country_datum>=[];
-
-    for (var i=0; i<init_data.features.length; i++) {
-      unvisited_countries.push({
-        "id": init_data.features[i].id,
-        "title": init_data.features[i].properties.name,
-        "visited" : "Unvisited",
-      });
-    }
-
-
-      var groupData = [
-        {
-          "name": "Unvisited",
-          "color": chart.colors.getIndex(1),
-          "data": unvisited_countries,
-        },
-        {
-          "name": "Visited",
-          "color": chart.colors.getIndex(3),
-          "data": visited_countries,
-        },
-      ];
-
-
-      // Create a series for each group, and populate the above array
-      groupData.forEach(function(group) {
-        var series = chart.series.push(new am4maps.MapPolygonSeries());
-        series.name = group.name;
-        series.useGeodata = true;
-        var includedCountries: string[] = [];
-        group.data.forEach(function(country) {
-          includedCountries.push(country.id);
-          // excludedCountries.push(country.id);
-        });
-        series.include = includedCountries;
-
-        series.fill = am4core.color(group.color);
-
-        // By creating a hover state and setting setStateOnChildren to true, when we
-        // hover over the series itself, it will trigger the hover SpriteState of all
-        // its countries (provided those countries have a hover SpriteState, too!).
-        series.setStateOnChildren = true;
-        series.calculateVisualCenter = true;
-
-        // Country shape properties & behaviors
-        var mapPolygonTemplate = series.mapPolygons.template;
-        // Instead of our custom title, we could also use {name} which comes from geodata
-        mapPolygonTemplate.fill = am4core.color(group.color);
-        mapPolygonTemplate.fillOpacity = 0.8;
-        mapPolygonTemplate.nonScalingStroke = true;
-        mapPolygonTemplate.tooltipPosition = "fixed"
-
-        mapPolygonTemplate.events.on("over", function(event) {
-          // series.mapPolygons.each(function(mapPolygon) {
-          //   mapPolygon.isHover = true;
-          // })
-          event.target.isHover = false;
-          event.target.isHover = true;
-        })
-
-        mapPolygonTemplate.events.on("out", function(event) {
-          // series.mapPolygons.each(function(mapPolygon) {
-          //   mapPolygon.isHover = false;
-          // })
-          // mapPolygon.isHover = false;
-          event.target.isHover=false;
-        })
-
-        mapPolygonTemplate.events.on("hit", function(event) {
-          var clicked_object : LooseObject = event.target.dataItem.dataContext
-          console.log("clicked", clicked_object);
-          console.log(clicked_object.visited);
-          // console.log(event.target);
-          // console.log("clicked_obj",clicked_object);
-          pointClick(clicked_object.id, clicked_object.name, clicked_object.visited)
-          // console.log(event.target.parent);
-          // console.log(chart.series.values)
-          console.log(chart.series);
-          console.log(event.target);
-        })
-
-        function pointClick(id: string, name: string, visited: string) {
-          if (visited=="Visited") {
-            var ser = chart.series.values[1];
-            var ser2 = chart.series.values[0];
-          }
-          else {
-            var ser = chart.series.values[0];
-            var ser2 = chart.series.values[1];
-          }
-          console.log("series", ser);
-          var index= findIndex(ser.data, id);
-          console.log(index);
-
-          var value = ser.data.splice(index, 1)[0];
-          ser2.data.push(value);
-        };
-
-        function findIndex(series: Array<any>, id: any) {
-          const search_func = (element: any) => element.id == id;
-
-          return series.findIndex(search_func);
-        }
-
-        // States
-        var hoverState = mapPolygonTemplate.states.create("hover");
-        hoverState.properties.fill = am4core.color("#CC0000");
-
-        let activeState = mapPolygonTemplate.states.create("active");
-        activeState.properties.fill = chart.colors.getIndex(4);
-
-        // Tooltip
-        mapPolygonTemplate.tooltipText = "{title} ({id}): {visited}"; // enables tooltip
-        // series.tooltip.getFillFromObject = false; // prevents default colorization, which would make all tooltips red on hover
-        // series.tooltip.background.fill = am4core.color(group.color);
-
-        // MapPolygonSeries will mutate the data assigned to it,
-        // we make and provide a copy of the original data array to leave it untouched.
-        // (This method of copying works only for simple objects, e.g. it will not work
-        //  as predictably for deep copying custom Classes.)
-        series.data = JSON.parse(JSON.stringify(group.data));
-
-        console.log(chart);
-      });
-
+        var plus_incls=unvisited.include.slice();
+        plus_incls.push(clicked_place.id);
+        unvisited.include=plus_incls;
+      })
       
 
       // This auto-generates a legend according to each series' name and fill
       chart.legend = new am4maps.Legend();
-
-      // Legend styles
-      chart.legend.paddingLeft = 27;
-      chart.legend.paddingRight = 27;
-      chart.legend.marginBottom = 15;
-      chart.legend.width = am4core.percent(90);
-      chart.legend.valign = "bottom";
-      chart.legend.contentAlign = "left";
-
-      // Legend items
-       chart.legend.itemContainers.template.interactionsEnabled = true;
-
-      // console.log(chart);
-
-      // for (var i=0; i < chart.series.length ; i++) {
-
-      //   console.log("Series ",chart.series.values[i].name);
-
-      //   for (var j=0; j<chart.series.values[i].data.length; j++) {
-      //     console.log(chart.series.values[i].data[j]);
-      //   }
-      // }
 }
 
   componentWillUnmount() {
@@ -261,7 +114,7 @@ class Chart extends Component {
 
   render() {
     return (
-      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+      <div id="chartdiv" style={{ width: "100%", height: "95vh" }}></div>
     );
   }
 }
